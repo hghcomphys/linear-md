5da6b4a8b7d6546f02643315d30c180a8bbc29fc
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2025-12-20 18:09:53 +0100

    implemented cells list, added dynamics

--- md.cpp
+++ md.cpp
@@ -6,9 +6,22 @@
 #include <iostream>
 #include <sstream>
 
-const int Ns = 100;                              // output frequency
+#define SQR(x) ((x) * (x))
+#define INDEX(ic, nc) ((ic)[0] + (nc)[0] * ((ic)[1] + (nc)[1] * (ic)[2]))
+#define ITERATE_OVER_CELLS(iv, mv)                      \
+    for ((iv)[0] = 0; (iv)[0] < (mv)[0]; (iv)[0]++)     \
+        for ((iv)[1] = 0; (iv)[1] < (mv)[1]; (iv)[1]++) \
+            for ((iv)[2] = 0; (iv)[2] < (mv)[2]; (iv)[2]++)
+#define ITERATE_OVER_ATOMS(iv, mv) \
+    for ((iv) = 0; (iv) < (mv); ++(iv))
+#define ITERATE_OVER_DIM(iv) \
+    for ((iv) = 0; (iv) < 3; ++(iv))
+
+const double r_cut = 2.5;
 const double K_B = 8.617343e-5;                  // Boltzmann's constant in natural unit
 const double TIME_UNIT_CONVERSION = 1.018051e+1; // from natural unit to fs
+const int OUTPUT_FREQUENCY = 100;
+const int MAX_ATOMS_PER_CELL = 500;
 
 struct SimulationParameters
 {
@@ -17,20 +30,30 @@ struct SimulationParameters
     double temperature;
 };
 
+struct Atom
+{
+    double mass;
+    double position[3];
+    double velocity[3];
+    double force[3];
+};
+
+struct Cell
+{
+    int count;
+    std::vector<int> atomIndex;
+};
+
 struct System
 {
     int numberOfAtoms;
-    int numberOfUpdates;
+    std::vector<Atom> atoms;
+
+    int cellSizes[3];
+    int numberOfCellUpdates;
+    std::vector<Cell> grid;
 
     double box[9];
-    double potentialEnergy;
-
-    std::vector<double> mass;
-    std::vector<double> x, y, z;
-    std::vector<double> vx, vy, vz;
-    std::vector<double> fx, fy, fz;
-
-    std::vector<double> x0, y0, z0;
 };
 
 std::vector<std::string> getTokens(std::ifstream &input)
@@ -78,6 +101,7 @@ int getInt(std::string &token)
 
 void readXyz(System &sys, const std::string &filename)
 {
+    int n, d, d1, d2;
 
     std::ifstream input(filename);
     if (!input.is_open())
@@ -97,62 +121,43 @@ void readXyz(System &sys, const std::string &filename)
     std::cout << "Number of atoms = " << sys.numberOfAtoms << std::endl;
 
     // allocate memory
-    sys.mass.resize(sys.numberOfAtoms, 0.0);
-    sys.x.resize(sys.numberOfAtoms, 0.0);
-    sys.y.resize(sys.numberOfAtoms, 0.0);
-    sys.z.resize(sys.numberOfAtoms, 0.0);
-    sys.vx.resize(sys.numberOfAtoms, 0.0);
-    sys.vy.resize(sys.numberOfAtoms, 0.0);
-    sys.vz.resize(sys.numberOfAtoms, 0.0);
-    sys.fx.resize(sys.numberOfAtoms, 0.0);
-    sys.fy.resize(sys.numberOfAtoms, 0.0);
-    sys.fz.resize(sys.numberOfAtoms, 0.0);
-    sys.x0.resize(sys.numberOfAtoms, 0.0);
-    sys.y0.resize(sys.numberOfAtoms, 0.0);
-    sys.z0.resize(sys.numberOfAtoms, 0.0);
+    sys.atoms.resize(sys.numberOfAtoms);
 
     // line 2
     tokens = getTokens(input);
     if (tokens.size() != 9)
     {
-        std::cerr << "The second line of xyz.in should have 9 items." << std::endl;
+        std::cerr << "The second line of " << filename << " should have 9 items." << std::endl;
         exit(1);
     }
 
-    for (int d1 = 0; d1 < 3; ++d1)
-    {
-        for (int d2 = 0; d2 < 3; ++d2)
-        {
-            sys.box[d2 * 3 + d1] = getDouble(tokens[d1 * 3 + d2]);
-        }
-    }
-
     std::cout << "Box matrix H = " << std::endl;
-    for (int d1 = 0; d1 < 3; ++d1)
+    ITERATE_OVER_DIM(d1)
     {
-        for (int d2 = 0; d2 < 3; ++d2)
+        ITERATE_OVER_DIM(d2)
         {
+            sys.box[d1 * 3 + d2] = getDouble(tokens[d1 * 3 + d2]);
             std::cout << sys.box[d1 * 3 + d2] << " ";
         }
         std::cout << std::endl;
     }
 
     // starting from line 3
-    for (int n = 0; n < sys.numberOfAtoms; ++n)
+    ITERATE_OVER_ATOMS(n, sys.numberOfAtoms)
     {
         tokens = getTokens(input);
-        if (tokens.size() != 5)
+        if (tokens.size() < 5)
         {
             std::cerr << "The 3rd line and later of xyz.in "
                          "should have 5 items."
                       << std::endl;
             exit(1);
         }
-        // atom types not used
-        sys.x[n] = getDouble(tokens[1]);
-        sys.y[n] = getDouble(tokens[2]);
-        sys.z[n] = getDouble(tokens[3]);
-        sys.mass[n] = getDouble(tokens[4]);
+        ITERATE_OVER_DIM(d)
+        {
+            sys.atoms[n].position[d] = getDouble(tokens[d + 1]);
+        }
+        sys.atoms[n].mass = getDouble(tokens[4]);
     }
 
     input.close();
@@ -214,25 +219,33 @@ void readRun(SimulationParameters &params, const std::string &filename)
 
 double ComputeKineticEnergy(const System &sys)
 {
+    int n, d;
     double kineticEnergy = 0.0;
-    for (int n = 0; n < sys.numberOfAtoms; ++n)
+
+    ITERATE_OVER_ATOMS(n, sys.numberOfAtoms)
     {
-        double v2 = sys.vx[n] * sys.vx[n] + sys.vy[n] * sys.vy[n] + sys.vz[n] * sys.vz[n];
-        kineticEnergy += sys.mass[n] * v2;
+        double v2 = 0.0;
+        ITERATE_OVER_DIM(d)
+        {
+            v2 += sys.atoms[n].position[d] * sys.atoms[n].position[d];
+        }
+        kineticEnergy += sys.atoms[n].mass * v2;
     }
-    return kineticEnergy * 0.5;
+    return 0.5 * kineticEnergy;
 }
 
 void scaleVelocity(System &sys, const double T0)
 {
+    int n, d;
     const double temperature =
         ComputeKineticEnergy(sys) * 2.0 / (3.0 * K_B * sys.numberOfAtoms);
     double scaleFactor = sqrt(T0 / temperature);
-    for (int n = 0; n < sys.numberOfAtoms; ++n)
+    ITERATE_OVER_ATOMS(n, sys.numberOfAtoms)
     {
-        sys.vx[n] *= scaleFactor;
-        sys.vy[n] *= scaleFactor;
-        sys.vz[n] *= scaleFactor;
+        ITERATE_OVER_DIM(d)
+        {
+            sys.atoms[n].velocity[d] *= scaleFactor;
+        }
     }
 }
 
@@ -241,58 +254,63 @@ void initializeVelocity(System &sys, const double T0)
 #ifndef DEBUG
     srand(42);
 #endif
+
+    int n, d;
     double centerOfMassVelocity[3] = {0.0, 0.0, 0.0};
     double totalMass = 0.0;
-    for (int n = 0; n < sys.numberOfAtoms; ++n)
+
+    ITERATE_OVER_ATOMS(n, sys.numberOfAtoms)
     {
-        totalMass += sys.mass[n];
-        sys.vx[n] = -1.0 + (rand() * 2.0) / RAND_MAX;
-        sys.vy[n] = -1.0 + (rand() * 2.0) / RAND_MAX;
-        sys.vz[n] = -1.0 + (rand() * 2.0) / RAND_MAX;
-        centerOfMassVelocity[0] += sys.mass[n] * sys.vx[n];
-        centerOfMassVelocity[1] += sys.mass[n] * sys.vy[n];
-        centerOfMassVelocity[2] += sys.mass[n] * sys.vz[n];
+        totalMass += sys.atoms[n].mass;
+        ITERATE_OVER_DIM(d)
+        {
+            sys.atoms[n].velocity[d] = -1.0 + (rand() * 2.0) / RAND_MAX;
+            centerOfMassVelocity[d] += sys.atoms[n].mass * sys.atoms[n].velocity[d];
+        }
     }
-    centerOfMassVelocity[0] /= totalMass;
-    centerOfMassVelocity[1] /= totalMass;
-    centerOfMassVelocity[2] /= totalMass;
-    for (int n = 0; n < sys.numberOfAtoms; ++n)
+    ITERATE_OVER_DIM(d)
     {
-        sys.vx[n] -= centerOfMassVelocity[0];
-        sys.vy[n] -= centerOfMassVelocity[1];
-        sys.vz[n] -= centerOfMassVelocity[2];
+        centerOfMassVelocity[d] /= totalMass;
+    }
+
+    ITERATE_OVER_ATOMS(n, sys.numberOfAtoms)
+    {
+        ITERATE_OVER_DIM(d)
+        {
+            sys.atoms[n].velocity[d] -= centerOfMassVelocity[d];
+        }
     }
     scaleVelocity(sys, T0);
 }
 
-bool checkIfNeedUpdate(const System &sys)
-{
-    bool needUpdate = false;
-    for (int n = 0; n < sys.numberOfAtoms; ++n)
-    {
-        double dx = sys.x[n] - sys.x0[n];
-        double dy = sys.y[n] - sys.y0[n];
-        double dz = sys.z[n] - sys.z0[n];
-        if (dx * dx + dy * dy + dz * dz > 0.25)
-        {
-            needUpdate = true;
-            break;
-        }
-    }
-    return needUpdate;
-}
+// bool checkIfNeedUpdate(const System &sys)
+// {
+//     bool needUpdate = false;
+//     for (int n = 0; n < sys.numberOfAtoms; ++n)
+//     {
+//         double dx = sys.x[n] - sys.x0[n];
+//         double dy = sys.y[n] - sys.y0[n];
+//         double dz = sys.z[n] - sys.z0[n];
+//         if (dx * dx + dy * dy + dz * dz > 0.25)
+//         {
+//             needUpdate = true;
+//             break;
+//         }
+//     }
+//     return needUpdate;
+// }
 
-void applyPbcOne(double &sx)
-{
-    if (sx < 0.0)
-    {
-        sx += 1.0;
-    }
-    else if (sx > 1.0)
-    {
-        sx -= 1.0;
-    }
-}
+// void applyPbcOne(double &sx)
+// {
+//     if (sx < 0.0)
+//     {
+//         sx += 1.0;
+//     }
+//     else if (sx > 1.0)
+//     {
+//         sx -= 1.0;
+//     }
+// }
 
 // void findNeighbor(System &sys)
 // {
@@ -308,6 +326,184 @@ void applyPbcOne(double &sx)
 //     }
 // }
 
+void initializeCells(System &sys)
+{
+    int d;
+    double l;
+
+    ITERATE_OVER_DIM(d)
+    {
+        l = sys.box[d * 3 + d];
+        sys.cellSizes[d] = (int)(l / r_cut);
+        if (sys.cellSizes[d] < 3)
+        {
+            std::cerr << "nc[" << d << "]=" << sys.cellSizes[d] << " under PBC, this must be greater than 2!" << std::endl;
+            exit(1);
+        }
+    }
+
+    int numberOfCells = 1;
+    ITERATE_OVER_DIM(d)
+    {
+        numberOfCells *= sys.cellSizes[d];
+    }
+    sys.grid.resize(numberOfCells);
+
+    for (Cell &cell : sys.grid)
+        cell.atomIndex.resize(MAX_ATOMS_PER_CELL);
+}
+
+void updateCells(System &sys)
+{
+    double l;
+    int n, d, ic[3];
+
+    ITERATE_OVER_CELLS(ic, sys.cellSizes)
+    {
+        sys.grid[INDEX(ic, sys.cellSizes)].count = 0;
+    }
+
+    ITERATE_OVER_ATOMS(n, sys.numberOfAtoms)
+    {
+        ITERATE_OVER_DIM(d)
+        {
+            l = sys.box[d * 3 + d];
+            ic[d] = (int)(sys.atoms[n].position[d] * sys.cellSizes[d] / l);
+        }
+
+        Cell &cell = sys.grid[INDEX(ic, sys.cellSizes)];
+        if (cell.count > MAX_ATOMS_PER_CELL)
+        {
+            std::cerr << "Max number of atoms per cell exceeded: " << MAX_ATOMS_PER_CELL << std::endl;
+            exit(1);
+        }
+        cell.atomIndex[cell.count++] = n;
+    }
+}
+
+void computeForce(System &sys)
+{
+    int d, ic[3], kc[3], jc[3];
+    double mag, rSqd, r[3];
+    double rInv2, rInv6, rInv12;
+    double l[3];
+
+    ITERATE_OVER_DIM(d)
+    {
+        l[d] = sys.box[d * 3 + d];
+    }
+
+    ITERATE_OVER_CELLS(ic, sys.cellSizes)
+    {
+        Cell &cell_ic = sys.grid[INDEX(ic, sys.cellSizes)];
+
+        // loop over atoms in cell ic
+        for (int i = 0; i < cell_ic.count; ++i)
+        {
+            int index_i = cell_ic.atomIndex[i];
+            ITERATE_OVER_DIM(d)
+            {
+                sys.atoms[index_i].force[d] = 0.0;
+            }
+
+            // iterate over neighbor cells of cell ic
+            for (kc[0] = ic[0] - 1; kc[0] <= ic[0] + 1; kc[0]++)
+            {
+                if ((kc[0] == -1) || (kc[0] == sys.cellSizes[0]))
+                    jc[0] = (kc[0] + sys.cellSizes[0]) % sys.cellSizes[0];
+                else
+                    jc[0] = kc[0];
+
+                for (kc[1] = ic[1] - 1; kc[1] <= ic[1] + 1; kc[1]++)
+                {
+                    if ((kc[1] == -1) || (kc[1] == sys.cellSizes[1]))
+                        jc[1] = (kc[1] + sys.cellSizes[1]) % sys.cellSizes[1];
+                    else
+                        jc[1] = kc[1];
+
+                    for (kc[2] = ic[2] - 1; kc[2] <= ic[2] + 1; kc[2]++)
+                    {
+                        if ((kc[2] == -1) || (kc[2] == sys.cellSizes[2]))
+                            jc[2] = (kc[2] + sys.cellSizes[2]) % sys.cellSizes[2];
+                        else
+                            jc[2] = kc[2];
+
+                        // iterate over all atoms in cell jc
+                        Cell &cell_jc = sys.grid[INDEX(jc, sys.cellSizes)];
+                        for (int j = 0; j < cell_jc.count; ++j)
+                        {
+                            int index_j = cell_jc.atomIndex[j];
+                            if (index_i != index_j)
+                            {
+                                rSqd = 0;
+                                ITERATE_OVER_DIM(d)
+                                {
+                                    r[d] = sys.atoms[index_i].position[d] - sys.atoms[index_j].position[d];
+                                    if (r[d] > l[d] * 0.5)
+                                        r[d] -= l[d];
+                                    else if (r[d] < -l[d] * 0.5)
+                                        r[d] += l[d];
+                                    rSqd += SQR(r[d]);
+                                }
+                                if (rSqd <= SQR(r_cut))
+                                {
+                                    rInv2 = 1. / rSqd;
+                                    rInv6 = rInv2 * SQR(rInv2);
+                                    rInv12 = SQR(rInv6);
+                                    mag = rInv2 * (48. * rInv12 - 24. * rInv6);
+                                    ITERATE_OVER_DIM(d)
+                                    {
+                                        std::cout << r[d] * mag << std::endl;
+                                        sys.atoms[index_i].force[d] += r[d] * mag;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void integrateVelocityVerlet1(System &sys, const SimulationParameters &params)
+{
+    int n, d;
+    double x;
+    double l[3];
+
+    ITERATE_OVER_DIM(d)
+    {
+        l[d] = sys.box[d * 3 + d];
+    }
+
+    ITERATE_OVER_ATOMS(n, sys.numberOfAtoms)
+    {
+        ITERATE_OVER_DIM(d)
+        {
+            x = sys.atoms[n].position[d] + params.timeStep * (sys.atoms[n].velocity[d] + params.timeStep * 0.5 / sys.atoms[n].mass * sys.atoms[n].force[d]);
+            if ((x < 0.) || (x >= l[d]))
+                x = fmod(x + 100. * l[d], l[d]);
+
+            sys.atoms[n].position[d] = x;
+            sys.atoms[n].velocity[d] += params.timeStep * 0.5 / sys.atoms[n].mass * sys.atoms[n].force[d];
+        }
+    }
+}
+
+void integrateVelocityVerlet2(System &sys, const SimulationParameters &params)
+{
+    int n, d;
+
+    ITERATE_OVER_ATOMS(n, sys.numberOfAtoms)
+    {
+        ITERATE_OVER_DIM(d)
+        {
+            sys.atoms[n].velocity[d] += params.timeStep * 0.5 / sys.atoms[n].mass * sys.atoms[n].force[d];
+        }
+    }
+}
+
 // int main(int argc, char **argv)
 int main()
 {
@@ -317,20 +513,24 @@ int main()
     readRun(params, "run.in");
     readXyz(sys, "Ar.xyz");
     initializeVelocity(sys, params.temperature);
+    initializeCells(sys);
+    updateCells(sys);
+    computeForce(sys);
 
-    std::cout << "Step Temperature KineticEnergy PotentialEnergy" << std::endl;
+    std::cout
+        << "Step Temperature KineticEnergy" << std::endl;
     for (int step = 0; step < params.numberOfSteps; ++step)
     {
-        // if (atom.neighbor_flag != 0)
-        //     findNeighbor(atom);
-        // integrate(true, timeStep, atom);  // step 1 in the book
-        // findForce(atom);                  // step 2 in the book
-        // integrate(false, timeStep, atom); // step 3 in the book
-        if (step % Ns == 0)
+        integrateVelocityVerlet1(sys, params);
+        updateCells(sys);
+        computeForce(sys);
+        integrateVelocityVerlet2(sys, params);
+
+        if (step % OUTPUT_FREQUENCY == 0)
         {
             const double kineticEnergy = ComputeKineticEnergy(sys);
-            const double T = kineticEnergy / (1.5 * K_B * sys.numberOfAtoms);
-            std::cout << step << " " << T << " " << kineticEnergy << " " << sys.potentialEnergy << std::endl;
+            const double T = kineticEnergy / (3.0 / 2.0 * K_B * sys.numberOfAtoms);
+            std::cout << step << " " << T << " " << kineticEnergy << std::endl;
         }
     }
 
